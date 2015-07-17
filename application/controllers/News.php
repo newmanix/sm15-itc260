85efbb605ba067e9d329adee12c7aee9d0daa0c2
Bill Newman <william.newman@seattlecolleges.edu>
2015-07-16 18:17:43 -0700

    Adds themes to our CI instance

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -6,6 +6,9 @@ class News extends CI_Controller {
         {
                 parent::__construct();
                 $this->load->model('news_model');
+            
+                $this->config->set_item('banner','News Banner');
+            
         }//end constructor
 
     public function index()
@@ -13,9 +16,9 @@ class News extends CI_Controller {
             $data['news'] = $this->news_model->get_news();
             $data['title'] = 'News archive';
 
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/index', $data);
-            $this->load->view('templates/footer');
+            //$this->load->view('templates/footer');
     }//end index()
 
     public function view($slug = NULL)
